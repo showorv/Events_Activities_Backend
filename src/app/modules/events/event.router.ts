3c49360c1ac3f5c605b42aeb7d73fbdae2bd1014
Showrov <yshowrov7@gmail.com>
2026-01-05 02:54:40 +0600

    changes

--- src/app/modules/events/event.router.ts
+++ src/app/modules/events/event.router.ts
@@ -15,13 +15,13 @@ router.post("/create",checkAuth(Role.HOST),multerUpload.single("file"), validate
 
 router.get("/hostEvent", checkAuth(Role.HOST), eventController.getOwnEventForHost)
 router.get("/adminEvent", checkAuth(Role.ADMIN, Role.SUPERADMIN), eventController.getAllEventForAdmin)
-router.get("/userEvent", checkAuth(...Object.values(Role)), eventController.getAllEventForUser)
+router.get("/userEvent",  eventController.getAllEventForUser)
 router.get("/userJoinedEvent", checkAuth(Role.USER), eventController.getAllJoinedEventForUser)
 router.get("/pendingPaymentEvent", checkAuth(Role.USER), eventController.getAllPendingPaymentsForUser)
 router.get("/revenue", checkAuth(Role.HOST), eventController.eventRevenue)
 
 
-router.get("/:id", checkAuth(...Object.values(Role)), eventController.getSingleEvent)
+router.get("/:id", eventController.getSingleEvent)
 
 router.get("/view/:id", checkAuth(Role.HOST, Role.SUPERADMIN, Role.ADMIN), eventController.viewParticipants)
 
